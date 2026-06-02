c79a56999bd1ef7c163e08dce985a2a103ef5dc5
Eli <elimermz@gmail.com>
2026-06-02 15:27:55 +0300

    wrote the get_soldier and get_soldiers_by_id

--- main.py
+++ main.py
@@ -7,3 +7,12 @@ app = FastAPI()
 @app.get("/soldiers")
 def get_soldiers():
     return json_to_list()
+
+
+@app.get("/soldiers/{id}")
+def get_soldier_by_id(id):
+    soldier_list = json_to_list()
+    for soldier in soldier_list:
+        if soldier["id"] == id:
+            return soldier
+    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Not Found")
