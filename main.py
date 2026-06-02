3dbaf92bc6ab136e19c35216f0608f5e7908bd31
Eli <elimermz@gmail.com>
2026-06-02 16:56:33 +0300

    added add,upadte and delete soldier

--- main.py
+++ main.py
@@ -10,9 +10,37 @@ def get_soldiers():
 
 
 @app.get("/soldiers/{id}")
-def get_soldier_by_id(id):
+def get_soldier_by_id(id: int):
     soldier_list = json_to_list()
     for soldier in soldier_list:
         if soldier["id"] == id:
             return soldier
     raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Not Found")
+
+
+@app.post("/soldiers")
+def add_soldier(new_soldier: dict):
+    soldier_list = json_to_list()
+    soldier_list.append(new_soldier)
+    list_to_json(soldier_list)
+    return {"message": "user added successfully"}
+
+
+@app.put("/soldiers/{id}")
+def update_soldier(id: int, new_soldier: dict):
+    soldier_list = json_to_list()
+    for soldier in soldier_list:
+        if soldier["id"] == id:
+            soldier.update(new_soldier)
+    list_to_json(soldier_list)
+    return {"message": "user updated successfully"}
+
+
+@app.delete("/soldiers/{id}")
+def delete_soldier(id: int):
+    soldier_list = json_to_list()
+    updated_soldier_list = [soldier for soldier in soldier_list if soldier["id"] != id]
+    if len(soldier_list) == len(updated_soldier_list):
+        raise HTTPException(404, "user not found")
+    list_to_json(updated_soldier_list)
+    return {"message": "soldier removed successfully"}
--- utils/io.py
+++ utils/io.py
@@ -4,6 +4,6 @@ def json_to_list():
     with open("soldiers.json", "r") as f:
         return json.load(f)
 
-def list_to_json(py_list, filename):
-    with open(filename, "w") as f:
+def list_to_json(py_list):
+    with open("soldiers.json", "w") as f:
         return json.dump(py_list, f, indent = 2)
